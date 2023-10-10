89c2b7f0d5263ed74c126d9490c0b86448fe0106
sara-mkhaled <sara.khaled393@yahoo.com>
2023-10-10 20:07:45 +0300

    fixed filter feature

---
+++ client/src/patient/Filter.jsx
@@ -0,0 +1,53 @@
+filterTest
+
+import React, { useState } from "react";
+import { medicines } from "../shared/assets/mockdata";
+import ProductCard from "../shared/components/Card";
+import ProductsGrid from "./MedList";
+import './style.css';
+
+
+const Filter = (props) => {
+    const [selectedMedicinalUse, setSelectedMedicinalUse] = useState("all");
+    // Initialize the filteredMedicines state variable with all medicines
+    //const [filteredMedicines, setFilteredMedicines] = useState(props.mappedArray);
+
+
+    const filterMedicinesByMedicinalUse = (medicinalUse) => {
+        setSelectedMedicinalUse(medicinalUse);
+      };
+    
+      const filteredArray = props.propArray.filter((medicine) => {
+        return (
+          medicine.extras.medicinalUse === selectedMedicinalUse ||
+          selectedMedicinalUse === "all"
+        );
+      });
+   
+    const mappedArray = filteredArray.map((medicine) => {
+        return (
+            <ProductCard
+                name={medicine.name}
+                description={medicine.description}
+                price={`$${medicine.price}`}
+                extras={medicine.extras}
+            />
+        );
+    });
+
+    return (
+        <div>
+            <div id="myBtnContainer">
+                <button className={`btn ${selectedMedicinalUse === "all" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('all')}>Show all</button>
+                <button className={`btn ${selectedMedicinalUse === "Antiviral" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('Antiviral')}>Antiviral</button>
+                <button className={`btn ${selectedMedicinalUse === "Antifungal" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('Antifungal')}>Antifungal</button>
+                <button className={`btn ${selectedMedicinalUse === "Antipyretic" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('Antipyretic')}>Antipyretic</button>
+                <button className={`btn ${selectedMedicinalUse === "Pain Reliever" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('Pain Reliever')}>Pain Reliever</button>
+                <button className={`btn ${selectedMedicinalUse === "Antibiotic" ? "active" : ""}`} onClick={() => filterMedicinesByMedicinalUse('Antibiotic')}>Antibiotic</button>
+            </div>
+            {mappedArray};               
+        </div>
+    );
+}
+
+export default Filter;
