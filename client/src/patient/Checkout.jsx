62eb1c518a8233eb2143d66cec78b71f0409978f
Menna Mohamed <mennamohamed132001@gmail.com>
2023-11-10 13:39:03 +0200

    checkout changes"

--- client/src/patient/Checkout.jsx
+++ client/src/patient/Checkout.jsx
@@ -6,7 +6,7 @@ const Checkout = (totalAmount) => {
   const savedAddresses = ['Address 1', 'Address 2', 'Address 3'];
   
   const [accordion2Open, setAccordion2Open] = useState(false);
-  const [accordion3Open, setAccordion3Open] = useState(true);
+  const [accordion3Open, setAccordion3Open] = useState(false);
 
   
   const handleAccordion2Continue = () => {
@@ -24,7 +24,7 @@ const Checkout = (totalAmount) => {
         <h1 style={{ textAlign: 'center' }}>Checkout</h1>
         <Accordion2 savedAddresses={savedAddresses} open={accordion2Open} setOpen={setAccordion2Open} onContinue={handleAccordion2Continue}/>  
         
-         <Accordion3 open={accordion3Open} setOpen={setAccordion3Open} totalAmount/>
+         <Accordion3 open={accordion3Open} setOpen={setAccordion3Open} totalAmount={totalAmount}/>
          
         
 
