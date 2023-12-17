c460f9e1633a21eb070e88d823f0eb1ddfad3e08
omarelkord <omarelkord12@gmail.com>
2023-12-17 05:29:59 +0200

    IGNORE previous commit

--- client/src/patient/PatientApp.js
+++ client/src/patient/PatientApp.js
@@ -12,36 +12,33 @@ import ViewSettings from "./scenes/ViewSettings";
 import ConnectAccountForm from "./scenes/ConnectAccountsForm";
 import PrescriptionsScreen from "./scenes/PrescriptionsScreen";
 import Chat from "../shared/pages/Chat";
+import Outline from "../shared/Outline";
+import sideBarItems from "./sideBarItems";
 
 function PatientApp({ socket }) {
 
-  import Outline from "../shared/Outline";
-  import sideBarItems from "./sideBarItems";
+  // console.log(data);
 
-  function PatientApp({ socket }) {
+  const outlet = <Routes>
+    <Route path="medicine" element={<MedList />} />
+    <Route path="medicine2" element={<MedicineScreen />} />
+    <Route path="checkout" element={<Checkout socket={socket} />} />
+    <Route path="/order" element={<OrderCard />} />
+    <Route path="/myOrders" element={<MyOrders />} />
+    <Route path="/medicine/alternativesScreen" element={<AlternativesScreen />} />
+    <Route path="/connectAccountForm" element={<ConnectAccountForm />} />
+    <Route path="/changePassword" element={<ChangePasswordScreen isPatient />} />
+    <Route path='/viewSettings' element={<ViewSettings />} />
+    <Route path='/prescription' element={<PrescriptionsScreen />} />
+    <Route path='/chat/:contact?' element={<Chat socket={socket} />} />
 
-    // console.log(data);
+    {/* </Route> */}
+  </Routes>
+  return (
+    <div>
+      <Outline style={{ backgroundColor: "blue" }} outlet={outlet} items={sideBarItems} socket={socket} isPatient />;
+    </div>
+  );
+}
 
-    const outlet = <Routes>
-      <Route path="medicine" element={<MedList />} />
-      <Route path="medicine2" element={<MedicineScreen />} />
-      <Route path="checkout" element={<Checkout socket={socket} />} />
-      <Route path="/order" element={<OrderCard />} />
-      <Route path="/myOrders" element={<MyOrders />} />
-      <Route path="/medicine/alternativesScreen" element={<AlternativesScreen />} />
-      <Route path="/connectAccountForm" element={<ConnectAccountForm />} />
-      <Route path="/changePassword" element={<ChangePasswordScreen isPatient />} />
-      <Route path='/viewSettings' element={<ViewSettings />} />
-      <Route path='/prescription' element={<PrescriptionsScreen />} />
-      <Route path='/chat/:contact?' element={<Chat socket={socket} />} />
-
-      {/* </Route> */}
-    </Routes>
-    return (
-      <div>
-        <Outline style={{ backgroundColor: "blue" }} outlet={outlet} items={sideBarItems} socket={socket} isPatient />;
-      </div>
-    );
-  }
-
-  export default PatientApp;
+export default PatientApp;
