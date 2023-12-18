7b3ce07283925fc42f892ffb64ceba09d9e6878d
ahmedlabib02 <ahmedlabibamer@gmail.com>
2023-12-19 01:47:04 +0200

    hatem al hatem abo hatem

--- client/src/shared/pages/GetStarted/GetStarted.js
+++ client/src/shared/pages/GetStarted/GetStarted.js
@@ -3,9 +3,10 @@ import GraphicalBackground from './GraphicalBackground';
 import RightHalf from './RightHalf';
 import { FloatButton } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router';
 
 export default function GetStarted({ task }) {   
-  
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col lg:flex-row min-h-screen">
       {/* Left side with background image and graphics */}
@@ -15,6 +16,12 @@ export default function GetStarted({ task }) {
       
       {/* Right side with form and information */}
       <RightHalf task={task} />
+
+      <FloatButton style={{
+      position: 'fixed', // Use fixed to position the button
+      top: '16px', // Distance from the top of the viewport
+      left: '16px', // Distance from the left of the viewport
+  }}  icon={<HomeOutlined/>} onClick={()=>(navigate('/'))} />
     </div>
   );
 }
