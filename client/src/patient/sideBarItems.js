cf98852619634acbd480476f139808588e447844
ahmedlabib02 <ahmedlabibamer@gmail.com>
2023-12-19 19:02:22 +0200

    vamo vamos

--- client/src/patient/sideBarItems.js
+++ client/src/patient/sideBarItems.js
@@ -31,23 +31,24 @@ const items = [
     label: 'View My Orders',
     to: '/patient/myOrders',
   },
+  
   {
     key: '3',
-    icon: <IoSettingsSharp />,
-    label: 'View Settings',
-    to: '/patient/viewSettings',
-  },
-  {
-    key: '4',
     icon: <GiMedicines />,
     label: 'Prescription',
     to: '/patient/prescription',
   },
   {
-    key: '5',
+    key: '4',
     icon: <FaHandHoldingMedical />,
     label: 'PharmaService',
     to: '/patient/chat/000000000000000000000000',
+  },
+  {
+    key: '5',
+    icon: <IoSettingsSharp />,
+    label: 'View Settings',
+    to: '/patient/viewSettings',
   },
   
 ].map(item => ({
